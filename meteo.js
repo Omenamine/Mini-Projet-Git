ec69fe015aaa183f5f20d70665c6dee1cbe624fc
Salimoxfive <95349780+Salimoxfive@users.noreply.github.com>
2022-05-20 17:53:04 +0200

    JS et background

---
+++ meteo.js
@@ -0,0 +1,20 @@
+function buttonClickGET() {
+    var queryLoc = document.getElementById("queryLoc").value;
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric"
+
+    $.get(url, callBackGetSuccess).done(function() {
+        //alert( "second success" );
+      })
+      .fail(function() {
+        alert( "error" );
+      })
+      .always(function() {
+        //alert( "finished" );
+      });
+}
+
+var callBackGetSuccess = function(data) {
+    console.log("donnes api",data)
+    var element = document.getElementById("zone_meteo");
+    element.innerHTML = "La temperature est de " + data.main.temp;
+}
